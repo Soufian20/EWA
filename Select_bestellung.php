409a0a493314521d45b5e409ba132cd039f70088
Soufian Asadfali <sasadfali@gmail.com>
2019-05-11 18:25:45 +0200

    Speisekarte wird nun über die Daten der Datenbank erstellt

--- Select_bestellung.php
+++ Select_bestellung.php
@@ -19,24 +19,50 @@ class Bestellung extends Page
 			throw new Exception("Abfrage fehlgeschlagen: ".$this->database->error);
 		        
 		// read selected records into result array
-		$pizza = array();
+		$pizzen = array();
 		$record = $recordset->fetch_assoc();
 		while ($record) {
-		    $pizza[] = $record["Pizzaname"];
+			//$pizzen[] = $record["PizzaName"];
+			$pizzen[] = $record;
 		    $record = $recordset->fetch_assoc();
 		}
 		$recordset->free();
-		return $pizza;
+		return $pizzen;
 	}
 
-
+private function insert_option($indent, $name){
+	echo ($indent."<option>".htmlspecialchars($name)."</option>\n");
+}
 
 protected function generateView() {
-    $pizza = $this->getViewData();
-    $numOfRecords = count($pizza);
+	$pizzen = $this->getViewData();
+	$this->generatePageHeader('Bestellung');
 
-    $this->generatePageHeader('Bestellung');
-    echo HI;
+    $numOfRecords = count($pizzen);
+	echo <<<EOT
+	<!-- NAVIGATIONSLEISTE -->
+         <nav class="header-nav"> 
+            <a href="index.php"><h1>Pi<span>zz</span>a</h1></a>
+            <ul>
+                <li><a href="index.php">Startseite</a></li>
+                <li class="active"><a href="Select_bestellung.php">Bestellung</a></li>
+                <li><a href="kunde.php">Kunde</a></li>
+                <li><a href="pizzabaecker.php">Pizzabäcker</a></li>
+                <li><a href="fahrer.php">Fahrer</a></li>
+            </ul>
+		</nav>
+		<!-- SPEISEKARTE -->
+        <section class="Speisekarte">
+		<h2>Speisekarte</h2>
+EOT;
+		foreach($pizzen as $pizza)
+		{
+			//$this->insert_option("\t\t", $pizza);
+			echo "<span class='gericht'>{$pizza["PizzaNummer"]} {$pizza["PizzaName"]} {$pizza["Preis"]}</span> <br>";
+       		echo "<img alt='{$pizza["PizzaName"]}' width='250' height='150' src='{$pizza["Bilddatei"]}'> <br>";
+		}
+		
+    
     $this->generatePageFooter();
 }
 
